372a8a9ef8b511004633221397d9fcfb24802a31
krazylink <paul.huckabee@gmail.com>
2013-07-19 17:08:46 -0400

    Added gflags and uritemplate. added userservice and cred storage to oauth_handler

--- main_handler.py
+++ main_handler.py
@@ -6,8 +6,9 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.write('Hello, World!')
-      self.response.write(dir(self))
       self.redirect('/auth')
+      
+      
 
 MAIN_ROUTES = [
     ('/', MainPage),
--- oauth_handler.py
+++ oauth_handler.py
@@ -2,14 +2,19 @@ import webapp2
 import httplib2
 
 from urlparse import urlparse
+from apiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
+from oauth2client.appengine import StorageByKeyName
+from model import Credentials
 
-SCOPES = ('https://www.googleapis.com/auth/glass.timeline')
+
+SCOPES = ('https://www.googleapis.com/auth/glass.timeline '
+          'https://www.googleapis.com/auth/userinfo.profile')
 
 
 class OAuthBaseRequestHandler(webapp2.RequestHandler):
-
+""" base handler for Oauth requests """
   def create_oauth_flow(self):
     flow = flow_from_clientsecrets('client_secrets.json', scope=SCOPES)
     parsed_url = urlparse(self.request.url)
@@ -18,9 +23,10 @@ class OAuthBaseRequestHandler(webapp2.RequestHandler):
 
 
 class OAuthCodeRequestHandler(OAuthBaseRequestHandler):
-
+""" handler for step1 of getting oauth creds """
   def get(self):
     flow = self.create_oauth_flow()
+    'This line is to make switching between local and prodction environments transparent'
     url = flow.step1_get_authorize_url(redirect_uri=flow.redirect_uri)
     self.redirect(str(url))
 
@@ -29,7 +35,6 @@ class OAuthExchangeRequestHandler(OAuthBaseRequestHandler):
 
   def get(self):
     code = self.request.get('code')
-    print code
     if not code:
       self.response.write('ERROR: Error getting auth code')
       return None
@@ -40,7 +45,13 @@ class OAuthExchangeRequestHandler(OAuthBaseRequestHandler):
     except FlowExchangeError:
       self.response.write('ERROR: %s' % (FlowExchangeError))
       return None
-    self.redirect('/')
+    http = httplib2.Http()
+    http = creds.authorize(http)
+    users_service = build('oauth2', 'v2', http=http)
+    user = users_service.userinfo().get().execute()
+    
+    userid = self.response.write(user.get('id'))
+    StorageByKeyName(Credentials, userid, 'credentials').put(creds)
 
 
 OAUTH_ROUTES = [
