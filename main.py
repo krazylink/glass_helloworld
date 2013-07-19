92a5a37ac391bc4c3c0fc60f0ef93826e69ef317
krazylink <paul.huckabee@gmail.com>
2013-07-19 12:18:25 -0400

    Added oauth handler. Added main. Moved helloworld.py to main_handler.py.

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0, 'lib')
+
+import webapp2
+
+from oauth_handler import OAUTH_ROUTES
+from main_handler import MAIN_ROUTES
+
+app = webapp2.WSGIApplication(ROUTES)
---
+++ oauth_handler.py
@@ -0,0 +1,44 @@
+import webapp2
+import httplib2
+
+from urlparse import urlparse
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+SCOPES = ('https://www.googleapis.com/auth/glass.timeline')
+
+class OAuthBaseRequestHandler(webapp2.RequestHandler):
+
+  def create_oauth_flow(self):
+    flow = flow_from_clientsecrets('client_secrets.json', scope=SCOPES)
+    parsed_url = urlparse(self.request.url)
+    flow.redirect_uri = '%s://%s/oauth2callback' % (parsed_url.scheme, parsed_url.netloc)
+    return flow
+
+class OAuthCodeRequestHandler(OAuthBaseRequestHandler):
+
+  def get(self):
+    flow = self.create_oauth_flow()
+    url = flow.step1_get_authorize_url()
+    self.recirect(str(url))
+
+class OAuthExchangeRequestHandler(OAuthBaseRequestHandler):
+
+  def get(self):
+    code = self.request.get('code')
+    if not code:
+      self.response.write('ERROR: Error getting auth code')
+      return None
+    oauth_flow = self.create_oauth_flow()
+
+    try:
+      creds = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+      self.response.write('ERROR: %s' % (FlowExchangeError))
+      return None
+    self.redirect('/')
+
+OAUTH_ROUTES = [
+    ('/auth', OAuthCodeRequestHandler),
+    ('/oauth2callback', OAuthExchangeRequestHandler),
+    ]
